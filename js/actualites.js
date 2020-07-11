6de92071b601eb37511a6111d6dfd6f120a2b065
yopox <louis.vignier@supelec.fr>
2020-07-11 15:50:53 +0200

    PLFR3 : rejet de propositions

--- js/actualites.js
+++ js/actualites.js
@@ -18,6 +18,9 @@ const actualites = {
     "PT3.2": [
         [Etats.JOKER, "29/06/2020 - Joker du président de la République.", "https://youtu.be/m0F-uslFshA?t=1277"],
     ],
+    "PT6.3": [
+        [Etats.ATTENTION, "09/07/2020 - Les engagements climatiques des entreprises relèvent du volontariat.", "https://reporterre.net/Budget-les-deputes-ne-prennent-pas-en-compte-les-mesures-de-la-Convention-climat"],
+    ],
     "PT12.1": [
         [Etats.ATTENTION, "30/06/2020 - Mme. Pannier-Runacher refuse le moratoire sur la 5G.", "https://youtu.be/2FW83wwuNZE?t=26"],
     ],
@@ -26,11 +29,21 @@ const actualites = {
     //     SE DÉPLACER
     // —————————————————————
 
+    "SD-A2.3": [
+        [Etats.ATTENTION, "09/07/2020 - Proposé lors du projet de loi de finances mais rejeté par la majorité.", "https://reporterre.net/Budget-les-deputes-ne-prennent-pas-en-compte-les-mesures-de-la-Convention-climat"],
+    ],
     "SD-A3.1": [
         [Etats.JOKER, "29/06/2020 - Joker du président de la République.", "https://youtu.be/m0F-uslFshA?t=1751"],
     ],
     "SD-A4.1": [
         [Etats.ATTENTION, "30/06/2020 - Bruno Le Maire se positionne contre la mesure.", "https://www.huffingtonpost.fr/entry/macron-bruno-le-maire-joker-convention-climat_fr_5efb7570c5b612083c53aa75"],
+        [Etats.ATTENTION, "09/07/2020 - Proposé lors du projet de loi de finances mais rejeté par la majorité.", "https://reporterre.net/Budget-les-deputes-ne-prennent-pas-en-compte-les-mesures-de-la-Convention-climat"],
+    ],
+    "SD-C1.2": [
+        [Etats.ATTENTION, "09/07/2020 - Proposé lors du projet de loi de finances mais rejeté par la majorité.", "https://reporterre.net/Budget-les-deputes-ne-prennent-pas-en-compte-les-mesures-de-la-Convention-climat"],
+    ],
+    "SD-C1.6": [
+        [Etats.ATTENTION, "09/07/2020 - Proposé lors du projet de loi de finances mais rejeté par la majorité.", "https://reporterre.net/Budget-les-deputes-ne-prennent-pas-en-compte-les-mesures-de-la-Convention-climat"],
     ],
     "SD-E2": [
         [Etats.ATTENTION, "29/06/2020 - Discours du PR : il en reste aux annonces déjà faites sur les trajets de moins de 2h30.", "https://youtu.be/m0F-uslFshA?t=1675"],
@@ -39,6 +52,10 @@ const actualites = {
     // —————————————————————
     //       SE LOGER
     // —————————————————————
+
+    "SL1.4": [
+        [Etats.ATTENTION, "09/07/2020 - Proposé lors du projet de loi de finances mais rejeté par la majorité.", "https://reporterre.net/Budget-les-deputes-ne-prennent-pas-en-compte-les-mesures-de-la-Convention-climat"],
+    ],
 
     // —————————————————————
     //      SE NOURRIR
